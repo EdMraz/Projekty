4ed74edf1c58dc65feb0f284e0fdaa2627d0835a
EdMraz <eduard.mraz05@gmail.com>
2022-09-21 22:10:19 +0200

    fixed code issues

--- uloha6.py
+++ uloha6.py
@@ -1,7 +1,9 @@
 print("Funkcia, ktorá zistí, či je dané číslo párne.")
 num = int(input("Zadaj číslo: "))
 
-if num%2 == 0:
+if num == 0:
+  print("Číslo musí byť väčšie ako nula.")
+elif num%2 == 0:
   print("Číslo je párne.")
 else:
   print("Číslo je nepárne.")
--- uloha8.py
+++ uloha8.py
@@ -2,9 +2,17 @@ print("Zisti, či je súčet dvoch posledných cifier párny.")
 
 num = str(input("Zadaj číslo: "))
 
+if len(num) < 2:
+    print("Číslo je jednociferné.")
+    exit()
+
 x = int(num[-1])
 y = int(num[-2])
-
 z = x + y
 
-print("Súčet dvoch posledných cifier je",z)
+if z%2 == 0:
+  str = "párne."
+else:
+  str = "nepárne."
+
+print("Súčet dvoch posledných cifier je",z,"toto číslo je",str)
